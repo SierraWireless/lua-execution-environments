278c93dd85505770029ad0afb0c3032b4fada424
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2012-04-26 15:32:10 +0200

    Adding last modules to LuaSocket

--- luasocket/2.0/mime.lua
+++ luasocket/2.0/mime.lua
@@ -15,6 +15,10 @@ To obtain the mime namespace, run:
  @module mime
 ]]
 
+--
+-- High-level filters
+--
+
 --[[----------------------------------------------------------------------------
  Converts most common end-of-line markers to a specific given marker.
 
@@ -70,6 +74,7 @@ To obtain the mime namespace, run:
  to chain it with your source, or apply it to your message body. 
  @function [parent=#mime] stuff
 ]]
+
 --[[----------------------------------------------------------------------------
  Returns a filter that breaks data into lines.
  
@@ -94,5 +99,172 @@ To obtain the mime namespace, run:
   In that case, the line length is fixed at 76. 
  @param #number length (optional) Available when _style_ is _"text"_, defaults
   76. 
+]]
+
+--
+-- Low-level filters
+--
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to perform Base64 encoding.
+
+ Note: The simplest use of this function is to encode a string into it's Base64
+ transfer content encoding. Notice the extra parenthesis around the call to
+ _mime.b64_, to discard the second return value. 
+	print((mime.b64("diego:password")))
+	--> ZGllZ286cGFzc3dvcmQ=
+
+ @function [parent=#mime] b64
+ @param #string C
+ @param D (optional)
+ @return With
+	A, B = mime.b64(C [, D])
+  _A_ is the encoded version of the largest prefix of _C..D_ that can be encoded
+  unambiguously. B has the remaining bytes of _C..D_, before encoding. If _D_ is
+  _#nil_, _A_ is padded with the encoding of the remaining bytes of _C_. 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to perform SMTP stuffing and enable transmission of messages containing the sequence "CRLF.CRLF". 
+
+ Note: The message body is defined to begin with an implicit CRLF. Therefore, to
+ stuff a message correctly, the first _m_ should have the value 2.
+	print((string.gsub(mime.dot(2, ".\r\nStuffing the message.\r\n.\r\n."), "\r\n", "\\n")))
+	--> ..\nStuffing the message.\n..\n..
+
+ Note: The @{smtp#smtp.send} function uses this filter automatically. You don't
+ need to apply it again.
+ 
+ @function [parent=#mime] dot
+ @param #number m
+ @param B (optional)
+ @return With
+	A, n = mime.dot(m [, B])
+  _A_ is the stuffed version of _B_. _'n'_ gives the number of characters from
+  the sequence CRLF seen in the end of _B_. _'m'_ should tell the same, but for
+  the previous chunk. 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to perform end-of-line marker translation.
+
+ For each chunk, the function needs to know if the last character of the
+ previous chunk could be part of an end-of-line marker or not. This is the
+ context the function receives besides the chunk. An updated version of the
+ context is returned after each new chunk.
+
+ @function [parent=#mime] eol
+ @param #number C The ASCII value of the last character of the previous chunk,
+  if it was a candidate for line break, or 0 otherwise. 
+ @param D (optional)
+ @param #string marker (optional) Gives the new end-of-line marker and defaults
+  to CRLF. 
+ @return With
+	A, B = mime.eol(C [, D, marker])
+  _A_ is the translated version of _D_. _C_ is the ASCII value of the last
+  character of the previous chunk, if it was a candidate for line break, or 0
+  otherwise. _B_ is the same as _C_, but for the current chunk. _Marker_ gives
+  the new end-of-line marker and defaults to CRLF.
+ @usage
+-- translates the end-of-line marker to UNIX
+unix = mime.eol(0, dos, "\n") 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to perform Quoted-Printable encoding.
+ 
+ Note: The simplest use of this function is to encode a string into it's
+ Quoted-Printable transfer content encoding. Notice the extra parenthesis around
+ the call to @{#mime.qp}, to discard the second return value.
+	print((mime.qp("maçã")))
+	--> ma=E7=E3=
+
+ @function [parent=#mime] qp
+ @param #string C
+ @param #string D (optional)
+ @param #string marker
+ @return With
+	A, B = mime.qp(C [, D, marker])
+ _A_ is the encoded version of the largest prefix of _C..D_ that can be encoded
+ unambiguously. _B_ has the remaining bytes of _C..D_, before encoding. If _D_
+ is _#nil_, _A_ is padded with the encoding of the remaining bytes of _C_.
+ Throughout encoding, occurrences of CRLF are replaced by the _marker_, which
+ itself defaults to CRLF.
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to break Quoted-Printable text into lines.
+
+ Note: Besides breaking text into lines, this function makes sure the line
+ breaks don't fall in the middle of an escaped character combination. Also, this
+ function only breaks lines that are bigger than _length_ bytes.
+ 
+ @function [parent=#mime] qpwrp
+ @param #number n
+ @param #string B (optional)
+ @param #number length (optional)
+ @return With
+	A, m = mime.qpwrp(n [, B, length])
+ _A_ is the decoded version of the largest prefix of _C..D_ that can be decoded
+ unambiguously. _B_ has the remaining bytes of C..D, before decoding. If _D_ is
+ _#nil_, A is the empty string and B returns whatever couldn't be decoded. 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to perform Base64 decoding.
+ 
+ Note: The simplest use of this function is to decode a string from it's Base64
+ transfer content encoding. Notice the extra parenthesis around the call to
+ @{#mime.unqp}, to discard the second return value.
+	print((mime.unb64("ZGllZ286cGFzc3dvcmQ=")))
+	--> diego:password
+
+ @function [parent=#mime] unb64
+ @param #string C
+ @param #string D (optional)
+ @return With
+	A, B = mime.unb64(C [, D])
+  _A_ is the decoded version of the largest prefix of _C..D_ that can be decoded
+  unambiguously. B has the remaining bytes of _C..D_, before decoding. If _D_ is
+  _#nil_, _A_ is the empty string and _B_ returns whatever couldn't be decoded. 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to remove the Quoted-Printable transfer content encoding from data.
+
+ Note: The simplest use of this function is to decode a string from it's
+ Quoted-Printable transfer content encoding. Notice the extra parenthesis around
+ the call to @#mime.unqp}, to discard the second return value.
+	print((mime.qp("ma=E7=E3=")))
+	--> maçã
+
+ @function [parent=#mime] unqp
+ @param C
+ @param D (optional)
+ @return With
+	print((mime.qp("ma=E7=E3=")))
+	--> maçã
+  _A_ is the decoded version of the largest prefix of _C..D_ that can be decoded
+  unambiguously. _B_ has the remaining bytes of _C..D_, before decoding. If _D_
+  is _#nil_, _A_ is augmented with the encoding of the remaining bytes of _C_. 
+]]
+
+--[[----------------------------------------------------------------------------
+ Low-level filter to break text into lines with CRLF marker.
+
+ Text is assumed to be in the @{#mime.normalize} form.
+ 
+ Note: This function only breaks lines that are bigger than _length_ bytes.
+ The resulting line length does not include the CRLF marker.
+ 
+ @function [parent=#mime] unqp
+ @param #number n
+ @param B (optional)
+ @param #number length (optional)
+ @return With
+	A, m = mime.wrp(n [, B, length])
+ _A_ is a copy of _B_, broken into lines of at most length bytes (defaults to
+ 76). _'n'_ should tell how many bytes are left for the first line of _B_ and
+ _'m'_ returns the number of bytes left in the last line of _A_. 
 ]]
 return nil
